754b24e1d0356b25c8e6f00900bc0da2642855d3
AkerkeKesha <akerke.kass@gmail.com>
2018-12-17 16:21:49 +0600

    poketimes with initial redux

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    posts: [
+        {id:'1', title:'Squirtle Laid an Egg', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '},
+        {id:'2', title:'Charmander Laid an Egg', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '},
+        {id:'3', title:'A Helix fossil was found', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '}
+    ]
+}
+
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducer;
